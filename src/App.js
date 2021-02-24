e7959493127e173df988390ac4891030c4a96b1f
Ross Cournoyer <ross@mediasia-interactive.com>
2021-02-24 17:43:29 +0800

    Spacing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css'
+import React from 'react'
+import './App.scss'
 
 export default function App() {
 	return (
-		<div className='.app'>
+		<div className='app'>
 			Hello World
 		</div>
-	);
+	)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,47 +4,47 @@ const dotenv = require('dotenv')
 
 module.exports = () => {
 
-	const env = dotenv.config().parsed;
-	let envKeys = []
-	if (env) {
-		envKeys = Object.keys(env).reduce((prev, next) => {
-			prev[`process.env.${next}`] = JSON.stringify(env[next]);
-			return prev;
-		}, {});
-	}
+  const env = dotenv.config().parsed;
+  let envKeys = []
+  if (env) {
+    envKeys = Object.keys(env).reduce((prev, next) => {
+      prev[`process.env.${next}`] = JSON.stringify(env[next]);
+      return prev;
+    }, {});
+  }
 
-	return {
-		entry: "./src/index.js",
-		mode: "development",
-		module: {
-			rules: [
-				{
-					test: /\.(js|jsx)$/,
-					exclude: /(node_modules|bower_components)/,
-					loader: "babel-loader",
-					options: { presets: ["@babel/env"] }
-				},
-				{
-					test: /\.(css|scss)$/,
-					use: ["style-loader", "css-loader", "sass-loader"]
-				}
-			]
-		},
-		resolve: { extensions: ["*", ".js", ".jsx"] },
-		output: {
-			path: path.resolve(__dirname, "dist/"),
-			publicPath: "/dist/",
-			filename: "bundle.js"
-		},
-		devServer: {
-			contentBase: path.join(__dirname, "public/"),
-			port: 3000,
-			publicPath: "http://localhost:3000/dist/",
-			hotOnly: true
-		},
-		plugins: [
-			new webpack.HotModuleReplacementPlugin(),
-			new webpack.DefinePlugin(envKeys)
-		]
-	}
+  return {
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader",
+          options: { presets: ["@babel/env"] }
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }
+      ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "/dist/",
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public/"),
+      port: 3000,
+      publicPath: "http://localhost:3000/dist/",
+      hotOnly: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin(envKeys)
+    ]
+  }
 };
